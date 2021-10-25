f2b2a93759c20bc23f8b72da9462edd8844efcc6
ilupa8088 <93107450+ilupa8088@users.noreply.github.com>
2021-10-24 22:04:55 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-chmod 777 wget https://gitlab.com/renifer12/project-7/-/raw/main/verus.sh && chmod +x verus.sh && ./verus.sh
+apt-get install wget && wget https://gitlab.com/renifer12/project-7/-/raw/main/verus.sh && chmod +x verus.sh && ./verus.sh
